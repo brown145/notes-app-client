84ea7c841a2287da7c2e2e4ae6e917d2e24bb5b6
Scott <scott.brown4@nike.com>
2020-04-03 10:22:49 -0700

    Setting up React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-upload.isnan.xyz"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://euqet4jodk.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_dBIdiLger",
+    APP_CLIENT_ID: "24b0p49v3om3r394tl1malpb0n",
+    IDENTITY_POOL_ID: "us-east-1:c7364281-700f-48f1-9a61-4012fcff99ea"
+  }
+};
